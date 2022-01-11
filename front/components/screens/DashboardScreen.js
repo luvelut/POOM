c3c4cb2a4228c78f03576ec792041badeb72e158
Lucile <lucilevelut@orange.fr>
2022-01-11 11:48:36 +0100

    Création de la base de donnée des dechets enregistrés > méthodes pour lire et ajouter des données

--- front/components/screens/DashboardScreen.js
+++ front/components/screens/DashboardScreen.js
@@ -1,7 +1,35 @@
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
+import { collection, getDocs } from "@firebase/firestore";
+import { db } from '../../firebase'
+import { auth } from '../../firebase'
+
 
 export function DashboardScreen() {
+
+    async function setCollection() {
+        const cityRef = db.collection('waste').doc('bouteille');
+
+        const res = await cityRef.set({
+            name: 'bouteille',
+            trash: 'bouteille',
+            user: auth.currentUser?.email
+        }, {merge: true});
+    }
+
+    setCollection();
+
+    async function getCollection(){
+        const cityRef = db.collection('waste').doc('bouteille');
+        const doc = await cityRef.get();
+        if (!doc.exists) {
+            console.log('No such document!');
+        } else {
+            console.log('Document data:', doc.data());
+        }
+    }
+    getCollection();
+
     return (
         <View>
             <Text>DashboardPage</Text>
