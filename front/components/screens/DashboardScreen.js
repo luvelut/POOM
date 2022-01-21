f564be2298c25966c63bffb7609b8d44e4f02359
Lucile <lucile@MacBook-Pro-de-Lucile.local>
2022-01-21 11:40:25 +0100

    Affichage de l'historique des déchets scannés

--- front/components/screens/DashboardScreen.js
+++ front/components/screens/DashboardScreen.js
@@ -1,24 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, FlatList} from 'react-native';
 import * as React from 'react';
-import { collection, getDocs } from "@firebase/firestore";
-import { db } from '../../firebase'
+import {db} from '../../firebase';
+import {auth} from '../../firebase';
+import {useEffect, useState} from 'react';
 
 export function DashboardScreen() {
+    const [tabData, setTabData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    async function getCollection(){
-        const cityRef = db.collection('waste').doc('bouteille');
-        const doc = await cityRef.get();
-        if (!doc.exists) {
-            console.log('No such document!');
-        } else {
-            console.log('Document data:', doc.data());
-        }
+    async function getUserWaste() {
+        return new Promise((resolve) => {
+            db.collection("waste").where("user", "==", auth.currentUser?.email)
+                .get()
+                .then((querySnapshot) => {
+                    const data = querySnapshot.map((doc) => doc.data());
+                    resolve(data);
+                }).catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
+        });
     }
-    getCollection();
 
+    const renderItem = ({item}) => <Text>{item.name}</Text>;
+
+    let isMounted = false;
+    useEffect(() => {
+        if (!isMounted) {
+            (async () => {
+                setTabData(await getUserWaste());
+                setIsLoading(false);
+            })();
+        }
+
+        return () => {
+            isMounted = true
+        }
+    }, []);
+
+    if (isLoading) return <></>
     return (
         <View>
             <Text>DashboardPage</Text>
+            <FlatList
+                data={tabData}
+                renderItem={renderItem}
+                keyExtractor={item => item.name}
+            />
         </View>
     );
 }
