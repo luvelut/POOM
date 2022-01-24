9f124b5d02115df17e2646f1727ab244226b31c6
Lucile <lucile@MacBook-Pro-de-Lucile.local>
2022-01-24 12:07:32 +0100

    Integration page dashboard + page liste des déchets

--- front/components/screens/DashboardScreen.js
+++ front/components/screens/DashboardScreen.js
@@ -1,12 +1,13 @@
-import {StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
+import {StyleSheet, Text, View, FlatList, ActivityIndicator, Alert, TouchableHighlight} from 'react-native';
 import * as React from 'react';
 import {db} from '../../firebase';
 import {auth} from '../../firebase';
 import {useEffect, useState} from 'react';
 import {Header} from '../common/Header';
 import { collection, query, where, getDocs } from "firebase/firestore";
+import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-export function DashboardScreen() {
+export function DashboardScreen({navigation}) {
     const [tabData, setTabData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     async function getUserWaste() {
@@ -25,7 +26,12 @@ export function DashboardScreen() {
         });
     }
 
-    const renderItem = ({item}) => <Text>{item.name}</Text>;
+    const renderItem = ({item}) => (
+        <View style={styles.item}>
+            <FontAwesome5 name="circle" size={70} color="#0000ff" />
+            <Text style={styles.itemTitle}>{item.name}</Text>
+        </View>
+    );
 
     let isMounted = false;
     useEffect(() => {
@@ -45,11 +51,52 @@ export function DashboardScreen() {
     return (
         <View>
             <Header/>
-            <FlatList
-                data={tabData}
-                renderItem={renderItem}
-                keyExtractor={item => item.name}
-            />
+            <View style={styles.content}>
+                <View style={styles.buttons}>
+                    <View style={[styles.button, styles.leftButton]}>
+                        <Text style={styles.buttonText}>Quantité économisées</Text>
+                    </View>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>Suivi composte</Text>
+                    </View>
+                </View>
+                <Text style={styles.title}>Badges</Text>
+                <View style={styles.badgeSection}>
+                    <View style={styles.badgeList}>
+                        <MaterialCommunityIcons style={styles.badgeItem} name="recycle-variant" size={30} color="#C49372" />
+                        <MaterialCommunityIcons style={styles.badgeItem} name="recycle-variant" size={30} color="#9B9B9B" />
+                        <MaterialCommunityIcons style={styles.badgeItem} name="recycle-variant" size={30} color="#FD5900" />
+                        <MaterialCommunityIcons style={styles.badgeItem} name="recycle-variant" size={30} color="#007FFD" />
+                    </View>
+                    <TouchableHighlight style={styles.icon}
+                                        underlayColor="#ffffff"
+                                        accessibilityRole="button"
+                                        onPress={() => {
+                                            Alert.alert("Pas encore disponible");
+                                        }
+                                        }>
+                        <Ionicons name="ios-add-outline" size={45} color="#9C9CD0" />
+                    </TouchableHighlight>
+                </View>
+                <Text style={styles.title}>Liste des déchets</Text>
+                <View style={styles.list}>
+                    <FlatList
+                        numColumns={tabData.length}
+                        data={tabData.slice(1,3)}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.name}
+                    />
+                    <TouchableHighlight style={styles.icon}
+                                        underlayColor="#ffffff"
+                                        accessibilityRole="button"
+                                        onPress={() => {
+                                            navigation.navigate('List', {tabWaste: tabData});
+                                        }
+                                        }>
+                        <Ionicons name="ios-add-outline" size={45} color="#9C9CD0" />
+                    </TouchableHighlight>
+                </View>
+            </View>
         </View>
     );
 }
@@ -59,9 +106,71 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center"
     },
-    horizontal: {
-        flexDirection: "row",
-        justifyContent: "space-around",
-        padding: 10
+    title: {
+        fontSize: 18,
+        paddingVertical: 5,
+        fontWeight: 'bold'
+    },
+    content: {
+        paddingHorizontal: 35,
+    },
+    item: {
+        alignItems: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 25,
+        backgroundColor: 'white',
+        marginVertical: 15,
+        marginRight: 15,
+    },
+    list: {
+        flexDirection: 'row',
+        alignItems: 'center'
+
+    },
+    icon: {
+        backgroundColor: 'white',
+        borderRadius: 40,
+        padding: 10,
+        marginLeft: 5
+    },
+    itemTitle: {
+        paddingTop: 5
+    },
+    badgeSection: {
+        flexDirection: 'row',
+        paddingBottom: 15,
+        alignItems: 'center'
+    },
+    badgeList: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 20,
+        paddingHorizontal: 15,
+        borderRadius: 25,
+        backgroundColor: 'white',
+        marginVertical: 15,
+        marginRight: 15,
+    },
+    badgeItem: {
+        paddingHorizontal: 9
+    },
+    buttons: {
+        flexDirection:'row',
+        paddingBottom: 10
+    },
+    button: {
+        backgroundColor: '#816BFD',
+        borderRadius: 25,
+        padding: 20,
+        alignItems: 'center',
+        alignContent: "center"
+    },
+    buttonText: {
+        color: 'white'
+    },
+    leftButton: {
+        width: 140,
+        marginRight: 25
     }
 });
